40360c2dc9d0f1f0fe129c5ed1c139b5f98a18cb
Eugene Lysenko <rus.advancedtester@gmail.com>
2022-12-12 00:11:41 +0300

    Exercise 5: JSONDecodeError is not raised

---
+++ Ex5.py
@@ -0,0 +1,15 @@
+from json.decoder import JSONDecodeError
+import json
+
+json_text = "messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}
+
+try:
+    json_text.json() #Пытаемся парсить сломаный json_text, чтобы получить JSONDecodeError
+    messages = json_text.get("messages")
+    second_message_text = messages[1].get("message")
+    print(f"Required message text is: {second_message_text}")
+except JSONDecodeError:
+    print("$\'json_text\' is not a JSON")
+except Exception as e:
+    print(str(e))
+
