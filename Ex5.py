128713e497683d5bd933a91f59735769be6214b5
Eugene Lysenko <rus.advancedtester@gmail.com>
2022-12-13 22:47:49 +0300

    Exercise 5: the very final version

--- Ex5.py
+++ Ex5.py
@@ -1,7 +1,8 @@
 from json.decoder import JSONDecodeError
 import json
 
-json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And ' \
+            'this is a second message","timestamp":"2021-06-04 16:41:01"}]} '
 
 try:
     obj = json.loads(json_text)
@@ -9,7 +10,7 @@ try:
     second_message_text = messages[1].get("message")
     print(f"Required message text is: {second_message_text}")
 except JSONDecodeError:
-    print("$\'json_text\' is not a JSON")
+    print("\'json_text\' is not a JSON")
 except Exception as e:
     print("Something went wrong. Google it using text below: " + str(e))
 
