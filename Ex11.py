6d174d2c8b63994d2cf89046c0a02271c5b04897
Eugene Lysenko <rus.advancedtester@gmail.com>
2022-12-26 23:31:34 +0300

    Exercise 11: tests for method /api/homework_cookie added. v1.1

--- Ex11.py
+++ Ex11.py
@@ -11,11 +11,12 @@ class TestCookies(BaseCase):
     response = requests.get(url)
 
     def test_endpoint_returns_cookie(self):
-        assert bool(self.response.cookies)
+        assert bool(self.response.cookies), f"Cookies was empty"
         for c in self.response.cookies:
             print(f"Cookie: {c.name}; Value: {c.value}")
 
     def test_cookie_has_correct_value(self):
         cookie = self.get_cookie(self.response, 'HomeWork')
-        assert cookie == "hw_value"
+        expected = "hw_value"
+        assert cookie == expected, f"Expected cookie value: {expected}, actual value was: {cookie}"
 
